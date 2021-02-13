e5dfdd16e8cc0d043a616b94b8effb1322344926
Oakleyvirgi.github.io <Molinav@byui.edu>
2021-02-13 18:53:46 -0500

    lesson 5 changes

--- lesson5/lesson5js/lesson5.js
+++ lesson5/lesson5js/lesson5.js
@@ -15,7 +15,7 @@ document.getElementById("date").innerHTML = dateStr;
 
 document.getElementById("copyright").innerHTML = year;
 
-if (day1[day] == 'Saturday') {
+if (day1[day] == 'Friday') {
   document.getElementById("onlyFriday").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
   document.getElementById("onlyFriday").style.display = "block";
 }
