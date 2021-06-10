ca7cf049f118efc6c4b16fdf84fbdea001bee10f
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-06-10 14:08:45 -0400

    icon added to the web tabs

--- lesson7/lesson7js/footer.js
+++
@@ -1,8 +0,0 @@
-let myYear = new Date();
-let myDate = myYear.getFullYear();
-
-document.getElementById("year").textContent = myDate;
-document.getElementById("lu").textContent =
-  "  Last Updated: " + document.lastModified;
-
- 
